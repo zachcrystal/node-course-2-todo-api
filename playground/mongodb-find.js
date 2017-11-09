8d83604885c40186814aa9c8aa9febd02c2a6f81
Zach Crystal <Zach@zachs-mbp.identification7.com>
2017-11-09 11:39:51 -0500

    Init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,33 @@
+// const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('5a026f955ab0f60792eee087'),
+  // }).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  // db.collection('Todos').find().count().then((count) => {
+  //   console.log(`Todos count: ${count}`);
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  db.collection('Users').find({ name: 'Zach' }).toArray().then((user) => {
+    console.log(user);
+  }, (err) => {
+    console.log('Unable to fetch todos', err);
+  });
+
+  db.close();
+});

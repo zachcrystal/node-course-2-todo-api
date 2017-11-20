f0bf07254b86258a442836b125002b4272f7452e
Zach Crystal <Zach@zachs-mbp.identification7.com>
2017-11-20 14:02:26 -0500

    add post /todos route and refactor mongoose

--- server/server.js
+++ server/server.js
@@ -1,38 +1,26 @@
-var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', {
-  useMongoClient: true,
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text,
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.send(e);
+  });
 });
 
-var Todo = mongoose.model('Todo', {
-  text: {
-    type: String,
-  },
-  completed: {
-    type: Boolean,
-  },
-  completedAt: {
-    type: Number,
-  },
-});
-
-// var newTodo = new Todo({
-//   text: 'Cook dinner',
-// });
-//
-// newTodo.save().then((doc) => {
-//   console.log('Saved todo', doc);
-// }, (e) => {
-//   console.log('Unable to save todo');
-// });
-
-var otherTodo = new Todo({
-
-});
-
-otherTodo.save().then((doc) => {
-  console.log(JSON.stringify(doc, undefined, 2));
-}, (e) => {
-  console.log('Unable to save', e);
+app.listen(3000, () => {
+  console.log('Started on port 3000');
 });

6e1999dcddaac98af1bf8c546c85b24a8402c883
Zach Crystal <Zach@Zachs-MacBook-Pro.local>
2017-11-21 18:11:11 -0500

    test POST /todos route

--- server/server.js
+++ server/server.js
@@ -17,10 +17,12 @@ app.post('/todos', (req, res) => {
   todo.save().then((doc) => {
     res.send(doc);
   }, (e) => {
-    res.send(e);
+    res.status(400).send(e);
   });
 });
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
+
+module.exports = { app };
